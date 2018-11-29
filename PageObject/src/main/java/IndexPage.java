222902d618262fd3972221c7d93a24f32f168e4d
Vsevolod <seva34land@mail.ru>
2018-11-29 22:31:14 +0300

    add lab page object

---
+++ PageObject/src/main/java/IndexPage.java
@@ -0,0 +1,52 @@
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class IndexPage {
+    WebDriver driver;
+
+    @FindBy(xpath = "//input[@placeholder='Откуда']")
+    private WebElement departureCity;
+
+    @FindBy(xpath = "//input[@placeholder='Куда']")
+    private WebElement arrivalCity;
+
+    @FindBy(xpath = "//label[contains(@class, 'form-radio is-white') and contains(.//span, 'В одну сторону')]")
+    private WebElement oneWay;
+
+    @FindBy(xpath = "//button[contains(@class, 'ui-state-highlight')]")
+    private WebElement todayDepartureDate;
+
+    @FindBy(id = "race-online")
+    private WebElement submitButton;
+
+    public IndexPage(WebDriver driver) {
+        this.driver = driver;
+        driver.get("https://www.rossiya-airlines.com");
+        PageFactory.initElements(driver, this);
+    }
+
+    public void setDepartureCity(String departure) {
+        departureCity.sendKeys(departure);
+    }
+
+    public void setArrivalCity(String arrival) {
+        arrivalCity.sendKeys(arrival);
+    }
+
+    public void setOneWay() {
+        oneWay.click();
+    }
+
+    public void setTodayDepartureDate() {
+        ((JavascriptExecutor) driver).executeScript("return document.getElementById('flight-start').click()");
+        todayDepartureDate.click();
+    }
+
+    public void submitForm() {
+        submitButton.click();
+    }
+}
---
+++ PageObject/src/test/java/RossiyaAirlines.java
@@ -0,0 +1,37 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class RossiyaAirlines {
+    WebDriver driver;
+
+    @BeforeClass
+    public void setupTest() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(
+                "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36");
+        options.addArguments("--start-maximized");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterClass
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void preventsBookingWithoutAgreeingWithTermsOfService() {
+        IndexPage index = new IndexPage(driver);
+        index.setDepartureCity("МОСКВА");
+        index.setArrivalCity("САНКТ-ПЕТЕРБУРГ");
+        index.setTodayDepartureDate();
+        index.submitForm();
+    }
+}
