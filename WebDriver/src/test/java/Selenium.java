89540a08ef7cf755cb24b71970ebd01f6217f657
Vsevolod <seva34land@mail.ru>
2018-11-29 22:24:42 +0300

    add .xml files

--- WebDriver/src/test/java/Selenium.java
+++ WebDriver/src/test/java/Selenium.java
@@ -13,27 +13,18 @@ import java.util.concurrent.TimeUnit;
 public class Selenium {
 
     @Test
-
-    public void test1() {
-
+    public void preventsEnteringNonexistentFlightNumberDuringOnlineCheckin() {
         WebDriver webDriver = new ChromeDriver();
-
         WebDriverWait wait = new WebDriverWait(webDriver, 5);
-
         webDriver.get("https://www.rossiya-airlines.com/");
-
         webDriver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-
         String mainTitle = webDriver.getTitle();
         WebElement element = webDriver.findElement(By.name("number"));
         element.sendKeys("5555");
         element.submit();
-
         webDriver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
-
         String afterTitle = "Поиск пассажира";
         Assert.assertTrue(mainTitle.equals(afterTitle));
-
         webDriver.quit();
     }
 }
